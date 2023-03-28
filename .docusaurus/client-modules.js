679c970b673a2797a78a966c9a13956be8eaa2c6
Aaron Honson <honsyb@gmail.com>
2023-03-28 20:11:16 +1030

    Testing Docusaurus
    To pair with Pterodocs to create a hosted editor

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/Users/a1702628/Documents/GitHub/org-smartstoryboard/wiki/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/a1702628/Documents/GitHub/org-smartstoryboard/wiki/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/a1702628/Documents/GitHub/org-smartstoryboard/wiki/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/Users/a1702628/Documents/GitHub/org-smartstoryboard/wiki/src/css/custom.css'),
+];
